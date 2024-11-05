07af81213a36ea165c23d644596fa095b44c0b5e
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-11-05 22:25:16 +0600

    refractoring

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 python test_time.py --cfg cfgs/imagenet_c/ours.yaml SETTING continual CORRUPTION.NUM_EX 50000
 python test_time.py --cfg cfgs/imagenet_c/ours.yaml SETTING mixed_domains CORRUPTION.NUM_EX 50000
-python test_time.py --cfg cfgs/imagenet_c/ours.yaml SETTING gradual CORRUPTION.NUM_EX 50000
+python test_time.py --cfg cfgs/imagenet_c/ours.yaml SETTING $djhf CORRUPTION.NUM_EX 50000
