3dae333d212bb3f7e2c729855fa6c8d45914e01c
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-06 17:15:37 +0000

    Reorganize optimizer steps to ensure zero_grad is called for optimizer_s and optimizer_backbone_t2

--- methods/ours.py
+++ methods/ours.py
@@ -274,16 +274,17 @@ class Ours(TTAMethod):
 
                 self.optimizer_t1.zero_grad()
                 self.optimizer_t2.zero_grad()
-                self.optimizer_s.zero_grad()
                 loss_div.backward(retain_graph=True)
-                loss_stu.backward()
                 self.optimizer_t1.step()
                 self.optimizer_t2.step()
-                self.optimizer_s.step()
 
-                self.optimizer_backbone_t2.zero_grad()
+                loss_stu.backward()
+                self.optimizer_s.step()
+                self.optimizer_s.zero_grad()
+
                 loss_t2.backward()
                 self.optimizer_backbone_t2.step()
+                self.optimizer_backbone_t2.zero_grad()
 
                 # self.optimizer_t1.zero_grad()
                 # self.optimizer_t2.zero_grad()
