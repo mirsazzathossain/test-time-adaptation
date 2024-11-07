c6cfe1fa437e36dc23bb3218c834d11a83dd5680
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-07 19:56:01 +0000

    Update loss configurations in run script to include KLD loss for CIFAR datasets

--- scripts/run.sh
+++ scripts/run.sh
@@ -26,20 +26,22 @@ python test_time.py --cfg cfgs/imagenet_others/ours.yaml SETTING continual_mixed
 python test_time.py --cfg cfgs/imagenet_others/ours.yaml SETTING correlated CORRUPTION.DATASET imagenet_a
 python test_time.py --cfg cfgs/imagenet_others/ours.yaml SETTING mixed_domains_correlated CORRUPTION.DATASET imagenet_a
 
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1,ce_s_t2,ce_s_aug_t1,contr_t2_proto,mse_t2_proto,contr_t2,im_loss,differ_loss SETTING continual
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1,ce_s_t2,ce_s_aug_t1,contr_t2_proto,mse_t2_proto,contr_t2,im_loss,differ_loss,mem_loss SETTING continual
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1,ce_s_t2,ce_s_aug_t1,contr_t2_proto,mse_t2_proto,contr_t2,im_loss SETTING continual
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1,ce_s_t2,ce_s_aug_t1,contr_t2_proto,mse_t2_proto,contr_t2 SETTING continual
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1,ce_s_t2,ce_s_aug_t1,contr_t2_proto,mse_t2_proto SETTING continual
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1,ce_s_t2,ce_s_aug_t1,contr_t2_proto SETTING continual
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1,ce_s_t2,ce_s_aug_t1 SETTING continual
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1,ce_s_t2 SETTING continual
-python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES ce_s_t1 SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto", "contr_t2", "im_loss", "differ_loss")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto", "contr_t2", "im_loss", "differ_loss", "mem_loss")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto", "contr_t2", "im_loss")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto", "contr_t2")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2")' SETTING continual
+python test_time.py --cfg cfgs/cifar10_c/ours.yaml Ours.LOSSES '("ce_s_t1")' SETTING continual
 
-python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "contr_t2", "im_loss", "differ_loss")' SETTING continual
-python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "contr_t2", "im_loss", "differ_loss", "mem_loss")' SETTING continual
-python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "contr_t2", "im_loss")' SETTING continual
-python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "contr_t2")' SETTING continual
+python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto", "contr_t2", "im_loss", "differ_loss")' SETTING continual
+python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto", "contr_t2", "im_loss", "differ_loss", "mem_loss")' SETTING continual
+python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto", "contr_t2", "im_loss")' SETTING continual
+python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto", "contr_t2")' SETTING continual
+python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto", "kld_t2_proto")' SETTING continual
 python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto", "mse_t2_proto")' SETTING continual
 python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1", "contr_t2_proto")' SETTING continual
 python test_time.py --cfg cfgs/cifar100_c/ours.yaml Ours.LOSSES '("ce_s_t1", "ce_s_t2", "ce_s_aug_t1")' SETTING continual
