828ff8180495e0fbe49ef7fb4527c35e701c13b1
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-11 21:56:22 +0000

    Add debug print statements for input and output sizes in Ours class

--- methods/ours.py
+++ methods/ours.py
@@ -294,7 +294,11 @@ class Ours(TTAMethod):
         correct_comb_t1_s = torch.argmax(comb_t1_s, dim=1) == y
         correct_comb_t2_s = torch.argmax(comb_t2_s, dim=1) == y
 
+        print(x.size())
+        print(outputs_t1.size())
+        print(logits_t1.size())
         print(correct_t1)
+
         total_correct_t1 = correct_t1.sum()
         total_correct_t2 = correct_t2.sum()
         total_correct_s = correct_s.sum()
