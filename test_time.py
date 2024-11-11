ad873415bc3bc4ff2cb43eb12805b85819d63c7e
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-11 23:12:23 +0000

    Update evaluation information in test_time.py to reflect current best model

--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "testing_models"
+    information = "current_best"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
