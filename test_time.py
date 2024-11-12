5218d8a6ed24ddf72fc5830aefc26fd89812734e
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-12 00:05:32 +0000

    Update evaluation information in test_time.py to include additional merged FID version

--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "merged_fid_1-2"
+    information = "merged_fid_1-2-3"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
