6fb417f7813e47cefdf6f1a54657699b25ea54d6
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-12 01:27:41 +0000

    Update evaluation information in test_time.py to reflect weighted ensemble method

--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "merged_fid_1-2-3"
+    information = "weighted_enssemble"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
