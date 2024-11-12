cdf689b248ebd79feabf248db54ebf1a6d8e491a
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-12 05:29:13 +0000

    Update evaluation information to reflect mmd_loss in test_time.py

--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "iftee_logic"
+    information = "mmd_loss"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
