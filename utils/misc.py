984cb4f4a296664b0208a3f976007d4b6577d13f
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-12 00:04:39 +0000

    Refactor confidence_condition function to improve filtering logic for entropy thresholds

--- utils/misc.py
+++ utils/misc.py
@@ -159,7 +159,12 @@ def confidence_condition(entropy_ema, entropy_ema2, entropy_threshold):
     # filter_ids.append(torch.where((entropy_ema < entropy_threshold) & (entropy_ema2 > entropy_threshold)))
     # filter_ids.append(torch.where((entropy_ema > entropy_threshold) & (entropy_ema2 < entropy_threshold)))
     # filter_ids.append(torch.where((entropy_ema > entropy_threshold) & (entropy_ema2 > entropy_threshold)))
-    filter_ids.append(torch.where((entropy_ema < entropy_threshold)))
+    combined_condition = (
+        ((entropy_ema < entropy_threshold) & (entropy_ema2 < entropy_threshold))
+        | ((entropy_ema < entropy_threshold) & (entropy_ema2 > entropy_threshold))
+        | ((entropy_ema > entropy_threshold) & (entropy_ema2 < entropy_threshold))
+    )
+    filter_ids.append(torch.where(combined_condition))
 
     return filter_ids
 
