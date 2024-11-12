618bea5ed20623cb8ada38cf76704013d775b6e4
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-12 02:02:38 +0000

    Uncomment loss calculations for mse_t2 and kld_t2 in Ours method

--- methods/ours.py
+++ methods/ours.py
@@ -400,10 +400,10 @@ class Ours(TTAMethod):
             loss_t2 += cntrs_t2_proto
             wandb.log({"contr_t2_proto": cntrs_t2_proto})
         if "mse_t2_proto" in self.cfg.Ours.LOSSES:
-            # loss_t2 += 10 * mse_t2
+            loss_t2 += 10 * mse_t2
             wandb.log({"mse_t2_proto": 10 * mse_t2})
         if "kld_t2_proto" in self.cfg.Ours.LOSSES:
-            # loss_t2 += 100 * kld_t2
+            loss_t2 += 100 * kld_t2
             wandb.log({"kld_t2_proto": 100 * kld_t2})
         if "contr_t2" in self.cfg.Ours.LOSSES:
             loss_t2 += cntrs_t2
