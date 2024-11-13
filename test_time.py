6924a5d6fe607205383a56084c91d6eb0858e944
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-13 10:01:38 +0000

    Update learning rate parameters in DomainShiftScheduler and modify evaluation run name information

--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "decrease_both"
+    information = "increase_both"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
