d4a0162d4f3565f4a3994374959f683b0b442a31
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-13 17:02:20 +0000

    Refactor Ours class by commenting out unused feature selection logic and update evaluation run name information

--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "add_1_forcefully"
+    information = "add_none_forcefully"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
