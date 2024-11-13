cca02a53d2540226f7e058075752905714195133
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-13 10:31:47 +0000

    Refactor Ours class by commenting out unused exponential moving average code and updating evaluation run name information

--- methods/ours.py
+++ methods/ours.py
@@ -154,8 +154,8 @@ class Ours(TTAMethod):
         #     5,
         # )
 
-        self.ema_prototypes = torch.zeros(num_classes, num_channels, device=self.device)
-        self.decay = 0.9
+        # self.ema_prototypes = torch.zeros(num_classes, num_channels, device=self.device)
+        # self.decay = 0.9
 
     def prototype_updates(
         self, pqs, num_classes, features, entropies, labels, selected_feature_id
@@ -348,12 +348,12 @@ class Ours(TTAMethod):
             selected_filter_ids,
         )
 
-        if self.c == 0:
-            self.ema_prototypes = prototypes.clone()
-        else:
-            self.ema_prototypes = (
-                self.decay * self.ema_prototypes + (1 - self.decay) * prototypes
-            )
+        # if self.c == 0:
+        #     self.ema_prototypes = prototypes.clone()
+        # else:
+        #     self.ema_prototypes = (
+        #         self.decay * self.ema_prototypes + (1 - self.decay) * prototypes
+        #     )
 
         if self.c % 200 == 0:
             logger.info(f"Number of empty queues: {self.is_pqs_full()}")
@@ -363,15 +363,15 @@ class Ours(TTAMethod):
         features_aug_t2 = self.backbone_t2(x_aug)
 
         cntrs_t2_proto = self.contrastive_loss_proto(
-            features_t2, self.ema_prototypes.detach(), labels_t1, margin=0.5
+            features_t2, prototypes.detach(), labels_t1, margin=0.5
         )
         mse_t2 = F.mse_loss(
-            features_t2, self.ema_prototypes[labels_t1].detach(), reduction="mean"
+            features_t2, prototypes[labels_t1].detach(), reduction="mean"
         )
-        kld_t2 = self.KL_Div_loss(features_t2, self.ema_prototypes.detach(), labels_t1)
+        kld_t2 = self.KL_Div_loss(features_t2, prototypes.detach(), labels_t1)
         cntrs_t2 = self.contrastive_loss(
             features_t2,
-            self.ema_prototypes.detach(),
+            prototypes.detach(),
             features_aug_t2,
             labels=None,
             mask=None,
@@ -483,14 +483,14 @@ class Ours(TTAMethod):
                                 1.0 - mask
                             )
 
-        with torch.no_grad():
-            if True:
-                prior = outputs.softmax(1).mean(0)
-                smooth = max(1 / outputs.shape[0], 1 / outputs.shape[1]) / torch.max(
-                    prior
-                )
-                smoothed_prior = (prior + smooth) / (1 + smooth * outputs.shape[1])
-                outputs *= smoothed_prior
+        # with torch.no_grad():
+        #     if True:
+        #         prior = outputs.softmax(1).mean(0)
+        #         smooth = max(1 / outputs.shape[0], 1 / outputs.shape[1]) / torch.max(
+        #             prior
+        #         )
+        #         smoothed_prior = (prior + smooth) / (1 + smooth * outputs.shape[1])
+        #         outputs *= smoothed_prior
 
         self.c = self.c + 1
         return outputs
--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "prototype_ema"
+    information = "best_mixed"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
