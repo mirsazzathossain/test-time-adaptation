2d1308e4cfa9c6d53e0182e8f47a4efdeb3acc68
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-13 23:07:57 +0000

    Implement exponential moving average for prototypes with a decay factor of 0.8 and update evaluation run name

--- methods/ours.py
+++ methods/ours.py
@@ -154,6 +154,9 @@ class Ours(TTAMethod):
         #     5,
         # )
 
+        self.ema_prototypes = torch.zeros(num_classes, num_channels, device=self.device)
+        self.decay = 0.8
+
     def prototype_updates(
         self, pqs, num_classes, features, entropies, labels, selected_feature_id
     ):
@@ -311,7 +314,7 @@ class Ours(TTAMethod):
             wandb.log({"ce_s_t1": loss_ce_s_t1.mean(0)})
         if "ce_s_t2" in self.cfg.Ours.LOSSES:
             loss_ce_s_t2 = self.symmetric_cross_entropy(outputs_s, outputs_t2.detach())
-            # loss_self_training += 0.5 * loss_ce_s_t2
+            loss_self_training += 0.5 * loss_ce_s_t2
             wandb.log({"ce_s_t2": loss_ce_s_t2.mean(0)})
         if "ce_s_aug_t1" in self.cfg.Ours.LOSSES:
             loss_ce_s_aug_t1 = self.symmetric_cross_entropy(
@@ -345,6 +348,13 @@ class Ours(TTAMethod):
             selected_filter_ids,
         )
 
+        if self.c == 0:
+            self.ema_prototypes = prototypes.clone()
+        else:
+            self.ema_prototypes = (
+                self.decay * self.ema_prototypes + (1 - self.decay) * prototypes
+            )
+
         if self.c % 200 == 0:
             logger.info(f"Number of empty queues: {self.is_pqs_full()}")
 
@@ -353,14 +363,18 @@ class Ours(TTAMethod):
         features_aug_t2 = self.backbone_t2(x_aug)
 
         cntrs_t2_proto = self.contrastive_loss_proto(
-            features_t2, prototypes.detach(), labels_t1, margin=0.5
+            features_t2, self.ema_prototypes.detach(), labels_t1, margin=0.5
         )
         mse_t2 = F.mse_loss(
-            features_t2, prototypes[labels_t1].detach(), reduction="mean"
+            features_t2, self.ema_prototypes[labels_t1].detach(), reduction="mean"
         )
-        kld_t2 = self.KL_Div_loss(features_t2, prototypes.detach(), labels_t1)
+        kld_t2 = self.KL_Div_loss(features_t2, self.ema_prototypes.detach(), labels_t1)
         cntrs_t2 = self.contrastive_loss(
-            features_t2, prototypes.detach(), features_aug_t2, labels=None, mask=None
+            features_t2,
+            self.ema_prototypes.detach(),
+            features_aug_t2,
+            labels=None,
+            mask=None,
         )
         im_loss = info_max_loss(outputs)
 
