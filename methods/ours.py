4716fe1ce9c87ed6b9ec94c94987ac2b991768c2
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-14 20:44:49 +0000

    Decrease batch size and learning rate for improved model performance; update logging information accordingly

--- methods/ours.py
+++ methods/ours.py
@@ -94,7 +94,7 @@ class Ours(TTAMethod):
             self.model_t2, self.arch_name, self.dataset_name
         )
         self.optimizer_backbone_t2 = self.setup_optimizer(
-            self.backbone_t2.parameters(), 0.01
+            self.backbone_t2.parameters(), 0.00025
         )
 
         # setup student model
--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "INC_increase_LR"
+    information = "INC_decrease_LR_t2"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
