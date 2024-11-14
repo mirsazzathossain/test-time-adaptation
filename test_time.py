3afd7594c75ed8b3f76a4f4596420893d688d2da
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-14 04:07:05 +0000

    Best model in INC

--- test_time.py
+++ test_time.py
@@ -35,7 +35,7 @@ def evaluate(description):
     # setup wandb logging
     wandb.run.name = cfg.MODEL.ADAPTATION + "-" + cfg.SETTING + "-" + cfg.CORRUPTION.DATASET
 
-    information = "output_t2"
+    information = "Best_INC"
     wandb.run.name += "-" + information
 
     # add current bangladesh time to the run name
