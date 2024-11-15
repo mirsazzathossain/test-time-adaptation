ae588f806e310ecf30780eae3aac11aea6d43182
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-11-15 00:48:01 +0000

    Remove batch normalization from model configuration and update wandb run name to reflect changes

--- methods/ours.py
+++ methods/ours.py
@@ -102,7 +102,7 @@ class Ours(TTAMethod):
             param.detach_()
 
         # configure student model
-        self.configure_model(self.model_s, bn=True)
+        self.configure_model(self.model_s)
         self.params_s, _ = self.collect_params(self.model_s)
         lr = self.cfg.OPTIM.LR
 
@@ -373,7 +373,7 @@ class Ours(TTAMethod):
 
         loss_t2 = 0.0
         if "contr_t2_proto" in self.cfg.Ours.LOSSES:
-            loss_t2 += cntrs_t2_proto
+            # loss_t2 += cntrs_t2_proto
             wandb.log({"contr_t2_proto": cntrs_t2_proto})
         if "mse_t2_proto" in self.cfg.Ours.LOSSES:
             loss_t2 += 10 * mse_t2
@@ -392,7 +392,7 @@ class Ours(TTAMethod):
             loss_l2_sp = L2SPLoss(pretrained_weights)
             l2_sp = loss_l2_sp(self.model_s)
             loss_stu += l2_sp
-            wandb.log({"l2_sp": l2_sp})
+            # wandb.log({"l2_sp": l2_sp})
         if "differ_loss" in self.cfg.Ours.LOSSES:
             # loss_stu += loss_differential
             wandb.log({"differ_loss": loss_differential})
